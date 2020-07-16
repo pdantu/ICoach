a3c605e6b4b353e352ad83c4fa56cbbafe00c4a9
pdant <pdantu1234@gmail.com>
2020-07-16 01:42:36 -0400

    finished Data class

--- app/src/main/java/com/example/icoach/Data.java
+++ app/src/main/java/com/example/icoach/Data.java
@@ -2,9 +2,31 @@ package com.example.icoach;
 
 public class Data
 {
-    public Data(int avgSpeed)
-    {
+    private int avgSpeed;
+    private int heading;
+    private int left;
+    private int right;
 
+    public Data(int avgSpeed, int heading, int left, int right) {
+        this.avgSpeed = avgSpeed;
+        this.heading = heading;
+        this.left = left;
+        this.right = right;
     }
 
+    public int getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    public int getHeading() {
+        return heading;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getRight() {
+        return right;
+    }
 }
