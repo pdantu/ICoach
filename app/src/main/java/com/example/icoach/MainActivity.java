f7d395ac9a38be139e6ea338648a9ade22371d83
pdant <pdantu1234@gmail.com>
2020-07-16 00:46:43 -0400

    Connected to FirebaseDatabase

--- app/src/main/java/com/example/icoach/MainActivity.java
+++ app/src/main/java/com/example/icoach/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase fb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
